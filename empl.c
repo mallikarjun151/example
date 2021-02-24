3798e7238f64a18fa9dd06e6c5211c29c08a90cc
mallikarjun151 <mallikarjun.shettar@covalensedigital.com>
2021-02-24 09:47:19 +0530

    dsdd

--- empl.c
+++ empl.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<string.h>
 #include<stdlib.h>
 struct emp{
     int empid;
@@ -7,15 +8,18 @@ struct emp{
 };
 typedef struct emp ll;
 ll emp[10];
-int main()
-{
-    for(int i=0;i<10;i++)
+int i;
+main()
+{   FILE *fp;
+    fp=fopen("employeedb","wb");
+    for(i=0;i<10;i++)
     {   printf("enter the empid\n");
         scanf("%d",&(emp[i].empid));
         printf("enter the empname\n");
-        scanf("%s",emp[i].empname));
+        scanf("%s",emp[i].empname);
         printf("enter the cmpny\n");
         scanf("%s",emp[i].cmpny);
+	fwrite(&emp[i],sizeof(emp[i]),1,fp);
     }
-    
+    fclose(fp);
 }
