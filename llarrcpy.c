6d882b81c087fe5c68dc6af5dafeb665e99ab47e
mallikarjun151 <mallikarjun.shettar@covalensedigital.com>
2021-02-28 11:06:04 +0530

    gd

--- llarrcpy.c
+++ llarrcpy.c
@@ -1,29 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 struct node
 { int data;
-  struct node *link;
+  struct node *link;  
 };
 typedef struct node ll; 
-ll* first=NULL;
-int stack[10];
-int choice=0,data,top=-1,size,v;
+
+ll *first=NULL,*temp,*llarr[4],*ptr;
+int arr[4][3];
+int choice=0,data,i=0,j=0,k;
 int main()
 {
 while(1)
-	{	printf("\n1.push\n2.strele\n3.altern\n4.stackele\n");
+	{	printf("\n1.create 2d arr\n2.Display arr\n3.create ll\n4.Display ll\n");
 		printf("enter the choice\n");
 		scanf("%d",&choice);
 		
 	 switch(choice)
 		{ 
-		 case 1:push();
+		 case 1:entr_arr();
 		 break;
-		 case 2:strele();
+		 case 2:arrele();
 		 break;
-		 case 3:altern();
+		 case 3:createll();
 		 break;
-		 case 4:stackele();
+		 case 4:llele();
 		 break;
 		 default:printf("wrong choice\n");
 		 exit(0);
@@ -31,79 +33,65 @@ while(1)
 		}
 	}
 }
-push()
-{ ll *temp;
- if(first==NULL)
-      {
-	first=(ll*)malloc(sizeof(ll));
-	printf("enter data\n");
-	scanf("%d",&first->data);
-	first->link=NULL;
-      }
-  else
- 	{
-	temp=(ll*)malloc(sizeof(ll));
-	printf("enter data\n");
-	scanf("%d",&temp->data);
-	temp->link=first;
-	first=temp;
-	}
- 
-}
-strele()
-{ll *temp;
- if(first==NULL)
-  {
-	printf("ll is empty\n");
-  }
- else
- {
-	temp=first;
- 	while(temp!=NULL)
-	{	
-		printf("[%d  %u]->\t",temp->data,temp->link);
-		temp=temp->link;
-		
-	}
- }
-}
-
-altern()
-{	ll *temp;
-	temp=(ll*)malloc(sizeof(ll));
-	int i=0;
-	if(first==NULL)
-	 {
-             printf("LL is empty\n");
-	 }
-	else
-	{
-		temp=first;
-		while(temp!=NULL)
-		{
-			if(i%2==0)
-			{ top++;
-			  stack[top]=temp->data;	
-			 }
-			temp=temp->link;
-		  
-		 i++;
-		}
-	}
-
-}
-stackele()
+entr_arr()
 {
- if(top==-1)
-  {  
-	 printf("stack is empty\n");
-  }
- else{
-   printf("stack ele r\n");
-   for(v=0;v<=top;v++)
-	{  printf("%d\t",stack[v]);
-	}
-   
-  }
-	top=-1;
+ printf("enter arr ele\n");
+ for(i=0;i<4;i++)
+   {
+	for(j=0;j<3;j++)
+	 {
+		scanf("%d",&arr[i][j]);
+	 }
+   }
 }
+arrele()
+{
+ printf("arr ele are\n");
+ for(i=0;i<4;i++)
+   {
+	for(j=0;j<3;j++)
+	{
+		printf("%d\t",arr[i][j]);
+	}
+	printf("\n");
+   }
+}
+
+
+createll()
+{ for(i=0;i<4;i++)
+     {
+ 	  
+	first=(ll*)malloc(sizeof(ll));
+	first->data=arr[i][0];
+	first->link=NULL;
+        llarr[i]=first;
+     	for(j=1;j<3;j++)
+  	  
+ 	     {
+		temp=(ll*)malloc(sizeof(ll));
+		temp->data=arr[i][j];
+		temp->link=NULL;
+		struct node *p;
+		p=llarr[i];
+		while(p->link!=NULL)
+		    { p=p->link;
+		    }
+		    p->link=temp;
+	    }
+     }
+}
+
+llele()
+{ 
+  for(i=0;i<4;i++)
+	{  ptr=llarr[i];
+	   while(ptr!=NULL)
+		{
+		 printf("%d\t",ptr->data);
+		 ptr=ptr->link;	
+		}
+	   printf("\n");
+	}
+}
+	
